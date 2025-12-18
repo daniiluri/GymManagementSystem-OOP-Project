716f65e23a312ef2b080b346dcbaa1c51b152f54
daniil <kimdanil0707@gmail.com>
2025-12-18 19:37:37 +0500

    Introduction to the bank system project

--- src/com/Daniil/bank/Main.java
+++ src/com/Daniil/bank/Main.java
@@ -1,9 +1,9 @@
 package com.Daniil.bank;
 
 public class Main {
-    static void main(String[] args) {
-        System.out.println("Hello, my name is Daniil Kim, and this is my first project in Java");
-        System.out.println("The theme of my project is banking system");
-        System.out.println("Here I will try to create a basic banking management system");
+    public static void main(String[] args) {
+        System.out.println("This is my first Project on Java");
+        System.out.println("The theme of my project is Gym system");
+        System.out.println("So here I will try to create a gym management system");
     }
 }
